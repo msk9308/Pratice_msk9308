a649d4d580bec04f95da8bfb0f29c4d5032b8afc
msk9308 <msk9308@naver.com>
2021-07-28 21:52:33 +0900

    pratice2

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/81/104289079aef001b1ef089d98939b009
@@ -0,0 +1,9 @@
+package kms01;
+
+public class run {
+	
+	public void run() {
+		System.out.println("달린다요");
+	}
+
+}
--- kms01/src/main/java/kms01/runmain.java
+++ kms01/src/main/java/kms01/runmain.java
@@ -1,11 +1,19 @@
 package kms01;
 
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class runmain {
 	
 	public static void main(String[] args) {
 		
-		run run = new run();
+//		run run = new run();
+//		run.run();
+		GenericApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		run run = ctx.getBean("run",run.class);
 		run.run();
+		
+		ctx.close();
 	}
 
 }
